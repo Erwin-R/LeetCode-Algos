31d6ce3aaccda52f5646985bdaf818406eebf508
Erwin-R <rerwin684@gmail.com>
2023-03-30 19:29:55 -0500

    added algo

--- Other/TwoSum.js
+++ Other/TwoSum.js
@@ -17,3 +17,28 @@ var twoSum = function(nums, target) {
 
     return outputArray
 };
+
+//this algo has O(n^2) time 
+
+
+var twoSum = function(nums, target) {
+    let map = {}
+
+    for(let i = 0; i < nums.length; i++){
+        let difference = target - nums[i]
+        
+        if(map[difference]){
+            return [i, map[difference] - 1]
+        }
+        
+        map[nums[i]] = i + 1
+        
+        
+
+    }
+
+
+
+};
+
+//this algo is O(n) time

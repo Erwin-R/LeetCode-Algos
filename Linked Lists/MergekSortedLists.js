d075a66d5cc27866c131d6bdf6d04a1c919d4523
Erwin-R <rerwin684@gmail.com>
2023-04-03 16:42:55 -0500

    added algo solution

--- Linked Lists/MergekSortedLists.js
+++ Linked Lists/MergekSortedLists.js
@@ -54,3 +54,49 @@ const mergeSort = (list1, list2) => {
 
     return newList.next
 }
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    let previous = null
+
+    for(let i = 0; i < lists.length; i++){
+        previous = mergeTwoLists(previous, lists[i])
+    }
+
+
+    return previous
+};
+
+
+
+var mergeTwoLists = function(list1, list2) {
+    let newList = new ListNode();
+    let current = newList
+
+    while(list1 && list2){
+        if(list1.val <= list2.val){
+            current.next = list1
+            list1 = list1.next
+        } else{
+            current.next = list2
+            list2 = list2.next
+        }
+        current = current.next
+    }
+
+    list1 ? current.next = list1 : current.next = list2
+
+    return newList.next
+};

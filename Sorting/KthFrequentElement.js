ccacbe61ed306c30c79e55f705196a21816bd2cd
Erwin-R <rerwin684@gmail.com>
2023-04-08 15:23:30 -0500

    added bucket sort algo

---
+++ Sorting/KthFrequentElement.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map = {}
+    let freq = new Array(nums.length + 1).fill([])
+
+    for(let i = 0; i < nums.length;i++){
+        map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1
+    }
+
+    for(let num in map){
+        freq[map[num]] = [...freq[map[num]], num]
+    }
+
+    let res = []
+
+    for(let i = freq.length - 1; i > 0; i--){
+        for(let num of freq[i]){
+            res.push(num);
+            if(res.length == k) return res
+        }
+    }
+};
+
+//with bucket sort it has O(n) time and O(n) space

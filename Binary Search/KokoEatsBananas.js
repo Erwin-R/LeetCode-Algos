88533493a0d773f0e405b367e38d028efd6a2995
Erwin-R <rerwin684@gmail.com>
2023-04-11 21:55:29 -0500

    added algo

--- Binary Search/KokoEatsBananas.js
+++ Binary Search/KokoEatsBananas.js
@@ -1,8 +1,15 @@
+piles =
+[1000000000,1000000000]
+h =
+3
 /**
  * @param {number[]} piles
  * @param {number} h
  * @return {number}
- */
+*/
+
+//O(n * m)
+
 var minEatingSpeed = function(piles, h) {
     let eatingSpeed = 1
 
@@ -22,4 +29,27 @@ var minEatingSpeed = function(piles, h) {
 
 };
 
-//O(n * m)
+//O(n * log(m))
+
+var minEatingSpeed = function(piles, h) {
+    let [left, right] = [1, Math.max(...piles)]
+    let res = right
+
+    while(left <= right){
+        let middle = Math.floor((left + right)/ 2)
+        let hoursSpent = 0
+
+        for(const pile of piles){
+            hoursSpent += Math.ceil(pile/middle)
+        }
+
+        if(hoursSpent <= h){
+            res = Math.min(res, middle)
+            right = middle - 1
+        } else{
+            left = middle + 1
+        }
+    }
+
+    return res
+};

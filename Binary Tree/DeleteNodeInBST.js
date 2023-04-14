57793492429eef20da7c40b5f165fe8b2cbaa9ce
Erwin-R <rerwin684@gmail.com>
2023-04-14 14:08:24 -0500

    added notes

--- Binary Tree/DeleteNodeInBST.js
+++ Binary Tree/DeleteNodeInBST.js
@@ -12,7 +12,7 @@
  * @return {TreeNode}
  */
 var deleteNode = function(root, key) {
-    const minValueNode = (root) => { // helper function
+    const minValueNode = (root) => { // helper function to find minimum value
         let curr = root
         while(curr && curr.left){
             curr = curr.left
@@ -22,17 +22,17 @@ var deleteNode = function(root, key) {
 
     if(!root) return null 
 
-    if(key < root.val){
-        root.left = deleteNode(root.left, key)
-    } else if(key > root.val){
+    if(key < root.val){ // used to find the val that we are going to delete
+        root.left = deleteNode(root.left, key) 
+    } else if(key > root.val){ // used to find the val that we are going to delete
         root.right = deleteNode(root.right, key)
     } else{
-        if(!root.left) return root.right
-        else if(!root.right) return root.left
+        if(!root.left) return root.right // these two lines are for cases where there is 1 or 0 children of the root
+        else if(!root.right) return root.left //
         else{
-            let minNode = minValueNode(root.right)
-            root.val = minNode.val
-            root.right = deleteNode(root.right, minNode.val)
+            let minNode = minValueNode(root.right) // we choose from the root.right because the greater values are on the right side on the root and we can to replace it with the smallest value on the right side.
+            root.val = minNode.val //now that we replaced root with the smallest right value we have duplicates 
+            root.right = deleteNode(root.right, minNode.val) //to remove the duplicate we run the function on the right node with minNode val as the key
         }
     }
 

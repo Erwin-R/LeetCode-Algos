0d211931253e9a05e5311472d90677c3405981af
Erwin-R <rerwin684@gmail.com>
2023-04-26 18:29:55 -0500

    added notes

--- Heaps/PriorityQueues/KthLargestElementInStream.js
+++ Heaps/PriorityQueues/KthLargestElementInStream.js
@@ -5,11 +5,13 @@
 var KthLargest = function(k, nums) {
     this.k = k
 
+    //set up min "heap" of k max numbers (k largest)
+    // so that way you can easily get the lowest kth element
     this.minHeap = new MinPriorityQueue()
 
-    nums.forEach(num => this.minHeap.enqueue(num)) 
+    nums.forEach(num => this.minHeap.enqueue(num)) //heapify them all 
 
-    while(this.minHeap.size() > this.k) this.minHeap.dequeue() 
+    while(this.minHeap.size() > this.k) this.minHeap.dequeue() //keeps just the top k elements
 };
 
 /** 
@@ -18,6 +20,13 @@ var KthLargest = function(k, nums) {
  */
 KthLargest.prototype.add = function(val) {
     this.minHeap.enqueue(val);
-    if(this.minHeap.size() > this.k) this.minHeap.dequeue() 
-    return this.minHeap.front().element 
+    if(this.minHeap.size() > this.k) this.minHeap.dequeue() //maintain top k 
+    return this.minHeap.front().element // top of heap = front of priority queue
+
+     //keeping top k elements in minHeap will result in the kth element being at the beginning of the heap
 };
+ //k = 2
+ //minHeap = [2,4,5,8]
+
+ //while loop (4 > 2) -> (3 > 2) -> (2 > 2)
+//minHeap = [4,5,8] -> [5,8] -> while loop stops

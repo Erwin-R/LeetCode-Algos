cecc2907e9b578716ae2d2901d44b365a1132851
Erwin-R <rerwin684@gmail.com>
2023-05-04 14:17:20 -0500

    added notes

---
+++ Hashing(Hashmap)/DesignHashmap.js
@@ -0,0 +1,6 @@
+/*
+"Chaining Method" 
+Time: O(N/K) where n is the number of possible keys and k is the predefined nodes inserted
+Space: O(K + M) where K is the number of predefined nodes in hashmap and M is the number
+of unique keys inserted into hashmap
+*/ 

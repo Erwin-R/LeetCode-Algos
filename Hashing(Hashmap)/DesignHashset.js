b5d8f5e3b3342b04463e9e271d91a02894763a8c
Erwin-R <rerwin684@gmail.com>
2023-05-07 14:51:50 -0500

    added notes

--- Hashing(Hashmap)/DesignHashset.js
+++ Hashing(Hashmap)/DesignHashset.js
@@ -1,3 +1,10 @@
+/*
+"Chaining Method" (open hashing: collisions stored outside of hash table)
+Time: O(N/K) where n is the number of possible keys and k is the predefined nodes inserted
+Space: O(K + M) where K is the number of predefined nodes in hashmap and M is the number
+of unique keys inserted into hashmap
+*/ 
+
 class ListNode{
     constructor(key = -1){
         this.key = key

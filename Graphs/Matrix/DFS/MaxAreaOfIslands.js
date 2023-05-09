a67790200353df6f87c6aa0b45cd8b7940cd2838
Erwin-R <rerwin684@gmail.com>
2023-05-09 12:34:55 -0500

    added algo

---
+++ Graphs/Matrix/DFS/MaxAreaOfIslands.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let maxArea = 0
+    let rows = grid.length, cols = grid[0].length
+
+    const dfs = (row, col) => {
+        if(Math.min(row, col) < 0 || row == rows || col == cols ||grid[row][col] == 0){
+            return 0
+        }
+
+        grid[row][col] = 0
+
+        return 1 + dfs(row + 1, col) +
+        dfs(row - 1, col) +
+        dfs(row, col + 1) + 
+        dfs(row, col - 1) 
+    }
+
+    for(let row = 0; row < rows; row++){
+        for(let col = 0; col < cols; col++){
+            if(grid[row][col] == 1){
+                maxArea = Math.max(maxArea, dfs(row, col))
+            }
+        }
+    }
+
+
+    return maxArea
+};

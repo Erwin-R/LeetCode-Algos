3b083c37bae46a7df0b35ff4afea0df9042720a4
Erwin-R <rerwin684@gmail.com>
2023-05-10 22:12:52 -0500

    added solution

--- Graphs/Matrix/BFS/ShortestPathwayInBinaryMatrix.js
+++ Graphs/Matrix/BFS/ShortestPathwayInBinaryMatrix.js
@@ -37,3 +37,37 @@ var shortestPathBinaryMatrix = function(grid) {
 };
 
 
+//Solution 2
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestPathBinaryMatrix = function(grid) {
+    let rows = grid.length, cols = grid[0].length
+    let queue = [[0, 0, 1]]
+    let directions = [[0,1], [0,-1], [1, 0], [-1, 0], [1, 1], [-1, -1] , [-1, 1], [1, -1]]
+    let visited = new Set(['0,0']);
+
+    while(queue.length){
+        let [row, col, length] = queue.shift()
+        if(Math.min(row, col) < 0 || row == rows || col == cols || grid[row][col] == 1){
+            continue
+        }
+        if(row == rows - 1 && col == cols - 1){
+            return length
+        }
+
+        for(const [dr, dc] of directions){
+            let [x,y] = [row + dr, col + dc]
+            let key = `${x},${y}`
+            if(!visited.has(key)){
+                queue.push([x, y, length + 1])
+                visited.add(key)
+            }
+        }
+
+    }
+
+
+    return -1    
+};

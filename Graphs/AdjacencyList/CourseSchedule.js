c54dcfd27669f5915b3029b5935617c89e5bc269
Erwin-R <rerwin684@gmail.com>
2023-05-16 11:39:22 -0500

    added notes

--- Graphs/AdjacencyList/CourseSchedule.js
+++ Graphs/AdjacencyList/CourseSchedule.js
@@ -38,13 +38,18 @@ var canFinish = function (numCourses, prerequisites) {
                 return false
             }
         }
+        
+        visitSet.delete(crs) //finished visiting and can take course
+        preMap[crs] = [] //if we visit this node again we know we can take prereq so set it to empty array
 
-        visitSet.delete(crs)
-        preMap[crs] = []
-
-        return true
+        return true //course can be taken 
     }
 
+
+    //have to manually iterate through courses like this incase we get a graph that is not entirely connected
+    // 1 -> 2, 1 has preReq of 2 
+    // 3 -> 4, 3 has preReq of 4
+    //iterating like this fixes this issue
     for (let i = 0; i < numCourses; i++) {
         if (!dfs(i)) {
             return false

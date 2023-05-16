e1fd66fd653d7403cc7cf2e2ca7e3ae8df42718c
Erwin-R <rerwin684@gmail.com>
2023-05-16 11:23:19 -0500

    added notes and solution

--- Dynamic Programming/ClimbingStairs.js
+++ Dynamic Programming/ClimbingStairs.js
@@ -1,3 +1,6 @@
+//Bottoms-Up approach 
+//Time: O(n), Space: O(1)
+
 var climbStairs = function(n) {
     let [one, two] = [1, 1] 
 
@@ -8,4 +11,20 @@ var climbStairs = function(n) {
     }
 
     return one
+};
+
+
+//Top-Down Approach (Using memoization)
+//Time O(n), Space: O(1)
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n, cache = {}) {
+    if(n <= 1) return n
+    if(cache[n]) return cache[n]
+
+    cache[n] = fib(n -1) + fib(n - 2)
+
+    return cache[n]
 };

92179312dd09d5c602735e6c45329ebe234f04e2
Erwin-R <rerwin684@gmail.com>
2023-05-19 14:55:25 -0500

    added solution

---
+++ Dynamic Programming/2-Dimensional DP/UniquePaths.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    prevRow = new Array(n).fill(0)
+
+    for(let i = m - 1; i > -1; i--){
+        currRow = new Array(n).fill(0)
+        currRow[n - 1] = 1
+        for(let i = n - 2; i > -1; i--){
+            currRow[i] = currRow[i + 1] + prevRow[i]
+        }
+        prevRow = currRow
+        console.log(prevRow)
+    }
+
+    return prevRow[0]
+};

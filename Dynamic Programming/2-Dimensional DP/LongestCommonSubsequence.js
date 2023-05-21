24c8169049fa40d7a35a4285672608d58fe71b55
Erwin-R <rerwin684@gmail.com>
2023-05-21 12:00:58 -0500

    added solution

---
+++ Dynamic Programming/2-Dimensional DP/LongestCommonSubsequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    let dp = new Array(text1.length + 1).fill(0).map(() => new Array(text2.length + 1).fill(0))
+
+    // console.log(dp)
+    
+    for(let i = text1.length - 1; i > -1; i--){
+        console.log(dp)
+        for(let j = text2.length - 1; j > -1; j--){
+            if(text1[i] == text2[j]){
+                dp[i][j] = 1 + dp[i + 1][j + 1]  
+            } else{
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1])
+            }
+        }
+    }
+
+    console.log(dp)
+
+    return dp[0][0]
+};

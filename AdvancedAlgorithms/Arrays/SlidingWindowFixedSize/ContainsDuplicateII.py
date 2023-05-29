783091a32ecd7e1eb8ca61d93a782ae6fa7e0e42
Erwin-R <rerwin684@gmail.com>
2023-05-28 21:38:05 -0500

    added notes and solution

--- AdvancedAlgorithms/Arrays/SlidingWindowFixedSize/ContainsDuplicateII.py
+++ AdvancedAlgorithms/Arrays/SlidingWindowFixedSize/ContainsDuplicateII.py
@@ -12,3 +12,25 @@ class Solution:
 
         
         return False
+
+
+#Optimized Solution
+#Time: O(n)
+#Space: O(n)
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+        L = 0
+
+        for R in range(len(nums)):
+            if R - L > k: #window is too big and need to remove first element we added to window which is at L
+                window.remove(nums[L])
+                L += 1
+
+            if nums[R] in window:
+                return True
+
+            window.add(nums[R])
+
+
+        return False

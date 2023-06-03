ba40b44da327a94da4648058bd5e660eb206407f
Erwin-R <rerwin684@gmail.com>
2023-06-03 16:05:52 -0500

    added notes and solution

--- AdvancedAlgorithms/Arrays/TwoPointers.js/ContainerWIthMostWater.py
+++ AdvancedAlgorithms/Arrays/TwoPointers.js/ContainerWIthMostWater.py
@@ -13,3 +13,23 @@ class Solution:
                 res = max(res, area)
 
         return res
+    
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        L, R = 0, len(height) - 1
+        res = 0 
+
+        while L < R: 
+            area = (R - L) * min(height[L], height[R])
+            res = max(res, area)
+
+            if height[L] < height[R]:
+                L += 1
+            # for this problem it does not matter which pointer we shift if height[L] == height[R]
+            else: 
+                R -= 1
+
+
+
+        return res

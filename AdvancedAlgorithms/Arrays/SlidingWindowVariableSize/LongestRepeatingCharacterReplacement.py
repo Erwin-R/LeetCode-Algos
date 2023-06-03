e3878cf268ddbd8ae9cf575990c50f3f8e636f00
Erwin-R <rerwin684@gmail.com>
2023-06-03 16:17:18 -0500

    added notes and solution

--- AdvancedAlgorithms/Arrays/SlidingWindowVariableSize/LongestRepeatingCharacterReplacement.py
+++ AdvancedAlgorithms/Arrays/SlidingWindowVariableSize/LongestRepeatingCharacterReplacement.py
@@ -21,3 +21,29 @@ class Solution:
 
 
         return res
+    
+
+#More optimized solution
+#Time: O(n) only keep track of most freq character
+#Space: O(1)
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {} #use map to keep track # of occurences of characters
+        res = 0
+
+        L = 0
+        maxF = 0
+        for R in range(len(s)):
+            count[s[R]] = 1 + count.get(s[R], 0) #counting instances
+            maxF = max(maxF, count[s[R]])
+
+            if (R - L + 1) - maxF > k:
+                #even though we decrement count of char, we are not decrementing maxF
+                count[s[L]] -= 1 
+                L += 1
+
+            res = max(res, R - L + 1)
+
+
+
+        return res

a2a364f6ccbd8f3b2ac488c901aa06b66c986c32
Erwin-R <rerwin684@gmail.com>
2023-06-04 17:58:19 -0500

    added notes

--- AdvancedAlgorithms/Arrays/TwoPointers.js/TwoSumII.py
+++ AdvancedAlgorithms/Arrays/TwoPointers.js/TwoSumII.py
@@ -1,10 +1,17 @@
+#Time: O(n)
+#Space: O(1)
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         L, R = 0, len(numbers) - 1
 
         while L < R: 
+            #if number is too big we wont reach our solution with
+            #values to the right of our pointer as they are bigger
             if numbers[L] + numbers[R] > target:
                 R -= 1
+            #wont reach our target with values to the left of L since they
+            #are smaller so move left pointer up
             elif numbers[L] + numbers[R] < target:
                 L += 1
             else: 

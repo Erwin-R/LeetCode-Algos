5adcd8e2164ee709a6a5f6eea7dc07d2b7192c82
Erwin-R <rerwin684@gmail.com>
2023-06-17 01:36:38 -0500

    added algo

---
+++ AdvancedAlgorithms/Trees/ImplimentTrie(PrefixTree).py
@@ -0,0 +1,24 @@
+par = {1: 2}
+
+p = par[1]
+print(p)
+
+#edges = [[1,2],[1,3],[2,3]]
+#par = [0, 1, 2, 3]
+#rank = [1, 1, 1, 1]
+
+
+#First Iteration 
+# par = [0, 2, 2, 3]
+# rank  [1, 1, 2, 1]
+
+#Second Iteration
+# par = [0, 2, 2, 2]
+# rank =[1, 1, 3, 1]
+
+#Third Iteration
+# hits first if and returns false
+
+# edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+# par = [0, 1, 2, 3, 4, 5]
+# rank = [1, 1, 1, 1, 1, 1]

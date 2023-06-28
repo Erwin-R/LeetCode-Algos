e4ae34b73e0c1ab6def06e06ad9b194cdedc47ec
Erwin-R <rerwin684@gmail.com>
2023-06-28 17:51:54 -0500

    added solution and notes

--- AdvancedAlgorithms/Trees/UnionFind/NumberOfConnectedComponentsinAnUndirectedGraph.py
+++ AdvancedAlgorithms/Trees/UnionFind/NumberOfConnectedComponentsinAnUndirectedGraph.py
@@ -46,3 +46,47 @@ class Solution:
                 count += 1
 
         return count 
+    
+    
+    
+#Union Find Solution
+
+#Storing size and parents of each vertex takes O(n) space
+#Space: O(n) 
+class UnionFind: 
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * (n)
+
+    def find(self, n):
+        while n != self.par[n]:
+            self.par[n] = self.par[self.par[n]]
+            n = self.par[n]
+        return n
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+
+        if p1 == p2: 
+            return 0
+
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]        
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+
+        return 1
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        uf = UnionFind(n)
+        res = n
+
+        for src, dst in edges: 
+            res -= uf.union(src, dst)
+
+        return res

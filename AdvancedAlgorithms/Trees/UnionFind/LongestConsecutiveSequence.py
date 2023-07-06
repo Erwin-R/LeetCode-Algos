d0acc72e9815aee21f044f93492c8074ec30c102
Erwin-R <rerwin684@gmail.com>
2023-07-05 20:32:46 -0500

    added solution

--- AdvancedAlgorithms/Trees/UnionFind/LongestConsecutiveSequence.py
+++ AdvancedAlgorithms/Trees/UnionFind/LongestConsecutiveSequence.py
@@ -18,3 +18,50 @@ class Solution:
 #Union Find solution
 #Time: O(n)
 #Space: O(n)
+
+class UnionFind: 
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, n):
+        while n != self.par[n]:
+            self.par[n] = self.par[self.par[n]]
+            n = self.par[n]
+        
+        return n
+
+
+    def union(self, x, y):
+        p1, p2 = self.find(x), self.find(y)
+        
+        if p1 == p2:
+            return
+
+        if self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        else: 
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        uf = UnionFind(len(nums))
+        #Initialize a dictionary to store the indices of all integers
+        index = dict()
+        
+        for i, num in enumerate(nums):
+            #Ignore duplicates
+            if num in index:
+                continue
+
+            index[num] = i
+
+            # Perform union with num - 1 and num + 1, if seen
+            if num - 1 in index: 
+                uf.union(i, index[num - 1])
+            if num + 1 in index: 
+                uf.union(i, index[num + 1])
+
+        return max(uf.rank + [0])

6fe4684a1172ad06907f5de35ff2ad573b7d2cfe
Erwin-R <rerwin684@gmail.com>
2023-07-18 10:32:03 -0500

    added algo

--- AdvancedAlgorithms/Backtracking/Combinations/Combinations.py
+++ AdvancedAlgorithms/Backtracking/Combinations/Combinations.py
@@ -1,3 +1,8 @@
+#Given n numbers(1 - n), return all possible combinations
+# of size k. (n choose k math problem)
+#Time: O(k * 2^n) where k is size of array
+#Space: O(n)
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combs, curComb = [], []
@@ -10,10 +15,12 @@ class Solution:
             if i > n:
                 return
 
+            #decision to include nums[i]
             curComb.append(i)
             dfs(i + 1)
-
             curComb.pop()
+
+            #decision NOT to include nums[i]
             dfs(i + 1)
 
         dfs(1)

10abd2d7a2c597e564b462108d28ecc93beefcaf
Erwin-R <rerwin684@gmail.com>
2023-07-22 10:19:09 -0500

    added solution

--- AdvancedAlgorithms/Backtracking/Permutations/Permutations.py
+++ AdvancedAlgorithms/Backtracking/Permutations/Permutations.py
@@ -2,6 +2,7 @@
 #Time: O(n! * n^2)
 #Space: O(n)
 
+#Recursive Solution
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         return self.helper(0, nums)
@@ -20,3 +21,20 @@ class Solution:
                 resPerms.append(pCopy)
         
         return resPerms
+    
+
+#Iterative Solution
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms = [[]] 
+
+        for n in nums:
+            nextPerms = []
+            for p in perms:
+                for i in range(len(p) + 1):
+                    pCopy = p.copy()
+                    pCopy.insert(i, n)
+                    nextPerms.append(pCopy)
+            perms = nextPerms
+
+        return perms

b84f53bfd33f5fc306d8de90c27742a57689865c
Erwin-R <rerwin684@gmail.com>
2023-08-10 16:09:47 -0500

    added solution

---
+++ AdvancedAlgorithms/Graphs/TopologicalSort/CourseSchedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visit = set()
+
+        def dfs(crs):
+            if crs in visit:
+                return False
+
+            #no preReqs or preReqs left
+            if preMap[crs] == []:
+                return True
+
+            visit.add(crs)
+
+            for preReq in preMap[crs]:
+                if not dfs(preReq): return False
+
+                #The .remove(crs) was so confusing but I finally understood it. Simplest explanation: if we exit the for-loop inside dfs, we know that crs is a good node without cycles. 
+                #However, if it remained in the visited set, we could trip the first if-clause in the dfs function if we revisit it and return False. 
+                #That's what we don't want to do, because we just calculated that crs has no cycles. So we remove it from visited so that other paths can successfully revisit it.
+                #Basically we can visit the node twice without it being a cycle due to it terminating multiple paths.
+            visit.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+
+        return True

034a0ebd08450c82d0501483962f1318ed736679
Erwin-R <rerwin684@gmail.com>
2023-08-28 10:15:26 -0500

    added algo

--- AdvancedAlgorithms/DynamicProgramming/PartitionEqualSubsetSum.py
+++ AdvancedAlgorithms/DynamicProgramming/PartitionEqualSubsetSum.py
@@ -1,19 +1,26 @@
-def dfsHelper(i, profit, weight, capacity):
-    if i == len(profit):
-        return 0
+#Brute Force Approach
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        def dfs(nums, n, subsetSum):
+            #Base cases 
+            if subsetSum == 0: 
+                return True 
+            if n == 0 or subsetSum < 0:
+                return False 
+            
+            result = (dfs(nums, n - 1, subsetSum - nums[n - 1])
+                    or dfs(nums, n - 1, subsetSum))
+            
+            return result
+        
+        
+        #first find sum of array
+        arraySum = sum(nums)
 
-    # Skip item i
-    maxProfit = dfsHelper(i + 1, profit, weight, capacity)
+        #if array sum is odd, it cannot be partitioned into subsets
+        if arraySum % 2:
+            return False 
 
-    # Include item i
-    newCap = capacity - weight[i]
-    if newCap >= 0:
-        p = profit[i] + dfsHelper(i + 1, profit, weight, newCap)
-        print("i:", i)
-        print("maxProfit before:", maxProfit)
-        print('P:', p)
-        # Compute the max
-        maxProfit = max(maxProfit, p)
-    print("maxProfit after:", maxProfit)
-    print("---------")
-    return maxProfit
+        subsetSum = arraySum // 2 
+        n = len(nums)
+        return dfs(nums, n - 1, subsetSum)

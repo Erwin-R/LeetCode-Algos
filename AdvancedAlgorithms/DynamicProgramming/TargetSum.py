f7e5d249c11fd18f575b5de15918fd830c8d0ca5
Erwin-R <rerwin684@gmail.com>
2023-09-01 23:27:11 -0500

    added notes

--- AdvancedAlgorithms/DynamicProgramming/TargetSum.py
+++ AdvancedAlgorithms/DynamicProgramming/TargetSum.py
@@ -12,3 +12,19 @@ class Solution:
             return dfs(nums, i + 1, curSum + nums[i]) + dfs(nums, i + 1, curSum - nums[i])
 
         return dfs(0, 0)
+    
+# Why TLE
+# imagine nums = [1, 1, 1], target = -1, there are 3 ways we can get -1
+# [-1, -1, +1], [+1, -1, -1], [-1, +1, -1]
+# Brute force will do the following
+#                         (0, 0) # index, summ
+#                 +1/                   \-1
+#               (1, 1)                  (1, -1)
+#               /    \                /         \
+#           (2,2)     (2,0)        (2,0)         (2,-2)
+#          /     \    /     \      /   \         /     \
+#      (3,3)   (3,1) (3,1),(3,-1) (3,1) (3,-1)  (3,-1) (3,-3)
+# note that at the bottom we have 3 nodes with summ == -1, hence the answer
+# IMP: we compute the node (2,0) twice -- if we traverse the left first
+# memoize this result we won't have to traverse this branch in the right
+# sub-tree.

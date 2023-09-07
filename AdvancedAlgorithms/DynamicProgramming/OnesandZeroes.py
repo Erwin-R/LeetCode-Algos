7b2a7bbe643fe2c8cd63f477610ff0832278f4ff
Erwin-R <rerwin684@gmail.com>
2023-09-06 22:49:38 -0500

    added solution

--- AdvancedAlgorithms/DynamicProgramming/OnesandZeroes.py
+++ AdvancedAlgorithms/DynamicProgramming/OnesandZeroes.py
@@ -1,10 +1,22 @@
 class Solution:
-    def findMaxForm(self, strs: List[str], M: int, N: int) -> int:
-        # Memoization
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         dp = {}
 
+        #where is each string in the strs array is represented by i 
         def dfs(i, m, n):
             if i == len(strs):
                 return 0
-            if (i, m, n) in dp:
+            if (i, m, n) in dp: 
                 return dp[(i, m, n)]
+
+            mCnt, nCnt = strs[i].count("0"), strs[i].count("1")
+
+            dp[(i, m, n)] = dfs(i + 1, m, n)
+
+            #if there are still enough 0's and 1's to subtract
+            if mCnt <= m and nCnt <= n:
+                dp[(i, m, n)] = max(dp[(i, m, n)], 1 + dfs(i + 1, m - mCnt, n - nCnt))
+
+            return dp[(i, m, n)]
+
+        return dfs(0, m, n)

8f965391ace2c4f8cd07d457170976029a98eb85
Erwin-R <rerwin684@gmail.com>
2023-09-18 21:07:11 -0500

    added solution

--- AdvancedAlgorithms/DynamicProgramming/Unbounded Knapsack/CoinChange.py
+++ AdvancedAlgorithms/DynamicProgramming/Unbounded Knapsack/CoinChange.py
@@ -1,10 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def dfs(i, total):
-            if i == len(coins): 
-                return 0
-            
-            if total == amount:
-                return 1
-            #skip item i
-            minCoins = dfs(i + 1, total)
+        dp = [amount + 1] * (amount + 1 )
+        dp[0] = 0
+        
+        print(dp)
+        #outer loop is traversing dp array where a is Amount
+        for a in range(1, amount + 1):
+            #inner loop is traversing coin loop
+            for c in coins: 
+                #difference between amount and coin value is greater than 0
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        #if dp[amount] was never changed/updated during the loop then that means that there is no possible combination 
+        return dp[amount] if dp[amount] != amount + 1 else -1

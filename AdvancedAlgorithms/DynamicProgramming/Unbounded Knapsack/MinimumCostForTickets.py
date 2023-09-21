8f5c90899f0e57a8060f5477e855c094b4ab3c40
Erwin-R <rerwin684@gmail.com>
2023-09-21 13:05:04 -0500

    added solution

--- AdvancedAlgorithms/DynamicProgramming/Unbounded Knapsack/MinimumCostForTickets.py
+++ AdvancedAlgorithms/DynamicProgramming/Unbounded Knapsack/MinimumCostForTickets.py
@@ -1,3 +1,4 @@
+
 #Memoization Solution
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
@@ -20,3 +21,17 @@ class Solution:
                 dp[i] = min(dp[i], c + dfs(j))
             return dp[i]
         return dfs(0)
+
+#Dynamic Programming Solution
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = {}
+        for i in range(len(days) - 1, -1, -1):
+            dp[i] = float("inf")
+            for d, c in zip([1, 7, 30], costs): 
+                j = i
+                #while j is still in bounds of days array and day[j] is less than day[i] + day pass
+                while j < len(days) and days[j] < days[i] + d:
+                    j += 1
+                dp[i] = min(dp[i], c + dp.get(j, 0))
+        return dp[0]

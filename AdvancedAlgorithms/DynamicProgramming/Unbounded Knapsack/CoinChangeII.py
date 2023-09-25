2daf4ab4ffa2e6a1de3966cc7b1e669c63295f47
Erwin-R <rerwin684@gmail.com>
2023-09-24 23:09:57 -0500

    added solution

--- AdvancedAlgorithms/DynamicProgramming/Unbounded Knapsack/CoinChangeII.py
+++ AdvancedAlgorithms/DynamicProgramming/Unbounded Knapsack/CoinChangeII.py
@@ -35,3 +35,19 @@ class Solution:
                 if a - coins[i] >= 0:
                     dp[a][i] += dp[a - coins[i]][i]
         return dp[amount][0]
+
+#Optimized DP memory solution
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]

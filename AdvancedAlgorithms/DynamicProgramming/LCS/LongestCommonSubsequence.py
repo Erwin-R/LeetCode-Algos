24d10cd098fdc65b1d85bdb1b8abe427cc116e0d
Erwin-R <rerwin684@gmail.com>
2023-09-25 20:53:17 -0500

    added solution

--- AdvancedAlgorithms/DynamicProgramming/LCS/LongestCommonSubsequence.py
+++ AdvancedAlgorithms/DynamicProgramming/LCS/LongestCommonSubsequence.py
@@ -1,3 +1,4 @@
+#Brute Force Solution
 #Time: O(2^(n +  m))
 #Space: O(n + m) *length of tree
 class Solution:
@@ -10,5 +11,27 @@ class Solution:
                 return 1 + dfs(i1 + 1, i2 + 1)
             else: 
                 return max(dfs(i1 + 1, i2), dfs(i1, i2 + 1))
+        
+        return dfs(0,0)
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        N, M = len(text1), len(text2)
+        cache = [[-1] * M for _ in range(N)]
+
+        def dfs(i1, i2):
+            if i1 == len(text1) or i2 == len(text2):
+                return 0 
+            
+            if cache[i1][i2] != -1:
+                return cache[i1][i2]
+
+            if text1[i1] == text2[i2]:
+                cache[i1][i2] = 1 + dfs(i1 + 1, i2 + 1)
+            else: 
+                cache[i1][i2] = max(dfs(i1 + 1, i2), dfs(i1, i2 + 1))
+            
+            return cache[i1][i2]
         
         return dfs(0,0)

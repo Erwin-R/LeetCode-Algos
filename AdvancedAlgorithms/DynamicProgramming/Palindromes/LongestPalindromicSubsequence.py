f573e208fa23d5a8dbe3d152207706527b35f2d2
Erwin-R <rerwin684@gmail.com>
2023-10-08 23:57:11 -0500

    added solution

--- AdvancedAlgorithms/DynamicProgramming/Palindromes/LongestPalindromicSubsequence.py
+++ AdvancedAlgorithms/DynamicProgramming/Palindromes/LongestPalindromicSubsequence.py
@@ -1,4 +1,4 @@
-#Memoization Solution
+#Memoization Solution (TLE)
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         cache = {}
@@ -19,3 +19,20 @@ class Solution:
             dfs(i, i + 1)
         
         return max(cache.values())
+    
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+            #Taking string and reversing it to since it should be the same if letters match
+            return self.longestCommonSubsequence(s, s[::-1])
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        N, M = len(text1), len(text2)
+        dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+        for i in range(N):
+            for j in range(M):
+                if text1[i] == text2[j]:
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+        return dp[N][M]
